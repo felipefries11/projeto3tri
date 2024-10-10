24d38f730e39156f4f8451c808e98bc61081a681
felipefries11 <148083290+felipefries11@users.noreply.github.com>
2024-10-10 07:51:21 -0300

    .

--- perguntas.js
+++ perguntas.js
@@ -1,1 +1,35 @@
+criaCartao(
+    'Historia',
+    'Em que ano foi declarado a independencia?',
+    '1822',
+)
 
+criaCartao(
+    'Matemática',
+    'Quanto que é 10x10',
+    '100',
+)
+
+criaCartao(
+    'Fortnite',
+    'Em que ano foi lançado o Fortnite?',
+    '2017',
+)
+
+criaCartao(
+    'Seleção',
+    'Quem é o artilheiro da seleção brasileira?',
+    'Neymar Jr',
+)
+
+criaCartao(
+    'Champions League',
+    'Quem é o artilheiro da Champions League?',
+    'Cristiano Ronaldo',
+)
+
+criaCartao(
+    'atleTIBA',
+    'Qual o time tem mais vitorias no atleTIBA?',
+    'Coritiba',
+)
